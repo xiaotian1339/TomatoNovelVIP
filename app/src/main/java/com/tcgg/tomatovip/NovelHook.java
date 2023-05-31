ec81abab5bb19fd937a2c532e775560246ed7109
xiaotian1339 <stc1339@163.com>
2023-05-31 19:11:38 +0800

    会员到期2099-12-31 / 去除更新弹窗

---
+++ app/src/main/java/com/tcgg/tomatovip/NovelHook.java
@@ -0,0 +1,63 @@
+package com.tcgg.tomatovip;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class NovelHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (!loadPackageParam.packageName.equals("com.dragon.read")){
+            return;
+        }
+        XposedHelpers.findAndHookConstructor("com.dragon.read.user.model.VipInfoModel", loadPackageParam.classLoader
+                , String.class, String.class, String.class, boolean.class, new XC_MethodHook() {
+
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        super.beforeHookedMethod(param);
+                        Object[] args = param.args;
+                        String expireTime = (String) args[0];
+                        String isVip = (String) args[1];
+                        String leftTime = (String) args[2];
+                        boolean isAutoCharge = (boolean) args[3];
+
+                        XposedBridge.log("TomatoVIP : 番茄 Hook 前 :expireTime -" + expireTime + "\nisVip - " + isVip +
+                                "\n leftTime - " + leftTime + "\n isAutoCharge - " + isAutoCharge);
+
+                        args[0] = "4102415999";
+                        args[1] = "1";
+                        args[2] = "999999";
+                        args[3] = false;
+                        XposedBridge.log("TomatoVIP : 小天番茄小说vip信息hook完毕");
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        super.afterHookedMethod(param);
+                        Object[] args = param.args;
+                        String expireTime = (String) args[0];
+                        String isVip = (String) args[1];
+                        String leftTime = (String) args[2];
+                        boolean isAutoCharge = (boolean) args[3];
+
+                        XposedBridge.log("TomatoVIP : 番茄 Hook 后 :expireTime -" + expireTime + "\nisVip - " + isVip +
+                                "\n leftTime - " + leftTime + "\n isAutoCharge - " + isAutoCharge);
+                    }
+                });
+
+        XposedHelpers.findAndHookMethod("com.dragon.read.update.b", loadPackageParam.classLoader, "b", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                param.setResult(false);
+            }
+        });
+
+
+
+
+
+    }
+}
